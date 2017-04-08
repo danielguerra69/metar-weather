bb4f048becb533b982c16e0cee03eed0400c1a04
danielguerra69 <daniel.guerra69@gmail.com>
2017-04-08 21:28:42 +0200

    minor log output improvement

--- bin/docker-cmd.sh
+++ bin/docker-cmd.sh
@@ -12,7 +12,7 @@ while [ 1 ]; do
   # get the first set
   lftp -c mirror http://tgftp.nws.noaa.gov/data/observations/metar/cycles
   # do all the data
-  for x  in `ls cycles/*.TXT`; do echo -n $(date)" "$x ; cat $x | grep -E "^[A-Z]{4} " | sort -u | metar2elastic.py ; done
+  for x  in `ls cycles/*.TXT`; do echo -n $(date)" "$x" " ; cat $x | grep -E "^[A-Z]{4} " | sort -u | metar2elastic.py ; done
   # remove cycles
   rm -rf cycles
   # wait 2/3 hour
